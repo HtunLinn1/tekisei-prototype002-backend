1af6f29c6ca3c5b861ee226cb0b6354afa707f59
HtunLinn <htunlinn@mm.cybermissions.co.jp>
2022-04-06 19:08:31 +0900

    environment mail setting

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,13 @@ require_relative "application"
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+ActionMailer::Base.smtp_settings = {
+  :address => "noreply@cybermissions.co.jp",
+  :port => 587,
+  :user_name => ENV["SEND_MAIL_USER"],
+  :password => ENV["SEND_MAIL_PASS"],
+  :authentication => :plain,
+  :enable_starttls_auto => true,
+  :openssl_verify_mode => 'none' 
+}
