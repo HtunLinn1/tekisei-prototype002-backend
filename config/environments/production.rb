209afe6cd1efed494edb54ec3caec6048d663c9b
HtunLinn <htunlinn@mm.cybermissions.co.jp>
2022-04-07 14:19:12 +0900

    production mail setting

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,6 +64,7 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
   config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default_options = {from: 'no-reply@cybermissions.co.jp'}
   config.action_mailer.default_url_options = { host: 'https://tekisei-ruby-proto-backend.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
